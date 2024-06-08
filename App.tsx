ef1d7fa47c20a4ce86f208e062084f4efab25e16
Soh Hong Yu <sohhongyu@gmail.com>
2024-06-08 16:16:32 +0800

    Made a todo to test auth

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import {
-  Button,
-  View,
-  StyleSheet,
-  SafeAreaView,
-} from "react-native";
+import { Button, View, StyleSheet, SafeAreaView } from "react-native";
 import { Amplify } from "aws-amplify";
-import {
-  Authenticator,
-  useAuthenticator,
-} from "@aws-amplify/ui-react-native";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
 import outputs from "./amplify_outputs.json";
+import TodoList from "./src/TodoList";
 
 Amplify.configure(outputs);
 
@@ -28,8 +21,9 @@ const App = () => {
   return (
     <Authenticator.Provider>
       <Authenticator>
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
           <SignOutButton />
+          <TodoList />
         </SafeAreaView>
       </Authenticator>
     </Authenticator.Provider>
@@ -37,6 +31,10 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
   signOutButton: {
     alignSelf: "flex-end",
   },
