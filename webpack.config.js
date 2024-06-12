51bcad22d1eb54aff849c3c0f7c645796635409d
Soh Hong Yu <sohhongyu@gmail.com>
2024-06-12 13:53:40 +0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,28 @@ const path = require("path");
 
 module.exports = {
   entry: "./App.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
   resolve: {
     fallback: {
       crypto: require.resolve("crypto-browserify"),
     },
     extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/, // Add this rule if you also have JSX files
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
